9a47e0860f390673dcc30f150d96ee500b492e0e
bigRookie <85099849+bigRookie555@users.noreply.github.com>
2022-03-18 15:36:34 +0800

    Add files via upload

--- mmap/index_handle.h
+++ mmap/index_handle.h
@@ -45,7 +45,7 @@ namespace skx{
 			}
 			
 			int32_t get_block_data_offset() const{
-				return data_file_offset_;
+				return reinterpret_cast<IndexHeader*>(file_op_->get_map_data())->data_file_offset_;
 			}
 			
 		private:
